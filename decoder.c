0969caa78ecc66f74f24a3b140bff9562adc9d73
LaersonSaraivaVerissimo <laerson.verissimo@gmail.com>
2019-09-13 14:59:17 -0300

    ultimo commit

--- decoder.c
+++ decoder.c
@@ -3,15 +3,36 @@
 #include "decoder.h"
 
 
-/*int main(void)
+int main(void)
 {
+    printf("NMEA Sentence decoder\n\n");
+    printf("Do you want to:\n");
+    printf("\t1. Decode a NMEA sentence\n");
+    printf("\t2. Create a NMEA sentence(Not implemented yet)\n");
 
-    decode("$GPGGA,1234,5");
-    return 0;
-}*/
+    int option;
+
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+        printf("Type the NMEA sentence you want to decode: ");
+        char sentence [90];
+        scanf("%s", &sentence);
+        decode(sentence);
+        break;
+    
+    default:
+        break;
+    }
+}
 
 void decode(char sentence[])
 {
+
+    
+    
     if (! gpVer(sentence))
     {
         printf("%s\n", "Invalid sentence");
@@ -19,6 +40,7 @@ void decode(char sentence[])
 
     else
     {
+        
         char * data = dataType(sentence);
 
         if (!strcmp(data, "GGA,"))
@@ -64,7 +86,7 @@ char * dataType(char sentence[]) {
 }
 
 void gga(char sentence[]) {
-    char time [10] = "";
+    char time [20] = "";
     char latitude[20] = "";
     char polo = '\0';
     char longitude [20] = "";
@@ -85,13 +107,17 @@ void gga(char sentence[]) {
 
     int sentenceCounter = 0;
     int i = 0;
+
     //Time
     while (sentenceCounter < strlen(sentence) && sentence[sentenceCounter] != ',')
     {
         time[i] = sentence[sentenceCounter];
         sentenceCounter ++;
         i++;
+
+        
     }
+
 
 //latitude
     sentenceCounter ++;
@@ -101,7 +127,9 @@ void gga(char sentence[]) {
         latitude[i] = sentence[sentenceCounter];
         sentenceCounter++;
         i++;
+        
     }
+
 
     //polo
     sentenceCounter++;
@@ -110,8 +138,9 @@ void gga(char sentence[]) {
     {
         //Tem quer ser N ou S
         polo = sentence[sentenceCounter];
+        sentenceCounter++;
     }
-
+    
     //longitude
     sentenceCounter++;
     i = 0;
@@ -128,6 +157,7 @@ void gga(char sentence[]) {
     while (sentenceCounter < strlen(sentence) && sentence[sentenceCounter] != ',')
     {
         eastWest = sentence[sentenceCounter];
+        sentenceCounter++;
     }
 
     //gpsQuality
@@ -136,6 +166,7 @@ void gga(char sentence[]) {
     while (sentenceCounter < strlen(sentence) && sentence[sentenceCounter] != ',')
     {
         gpsQualityIndicator = sentence[sentenceCounter];
+        sentenceCounter++;
     }
 
     //numberOfSatellites
@@ -175,6 +206,7 @@ void gga(char sentence[]) {
     while (sentenceCounter < strlen(sentence) && sentence[sentenceCounter] != ',')
     {
         unitOfAltitude = sentence[sentenceCounter];
+        sentenceCounter++;
     }
 
     //Geoidal separation
@@ -194,6 +226,7 @@ void gga(char sentence[]) {
     while (sentenceCounter < strlen(sentence) && sentence[sentenceCounter] != ',')
     {
         unitOfGeoidalSeparation = sentence[sentenceCounter];
+        sentenceCounter++;
     }
 
     //dgpsAGE
@@ -226,6 +259,193 @@ void gga(char sentence[]) {
        sentenceCounter++;
        i++;
     }
+
+    //time of the signal
+    printf("Time of the position report (UTC): ");
+    for(i = 0; i < strlen(time); i++) {
+        if (i == 2 || i == 4)
+        {
+            printf(": ");
+        }
+        
+        printf("%c", time[i]);
+
+        switch (i)
+        {
+        case 1:
+            printf("h ");
+            break;
+
+        case 3:
+            printf("m ");
+            break;
+        
+        default:
+            break;
+        }
+
+        if (strlen(time) >= 6 && i == strlen(time) - 1)
+        {
+           printf("s\n\n");
+        }
+        
+    }
+
+    //latitude
+    int decimalPointIndex = -1;
+    for(i = 0; i < strlen(latitude); i ++) {
+        if(latitude[i] == '.') {
+            decimalPointIndex = i;
+        }
+        //se o decimalpoint não for encontrado, é erro de entrada
+    }
+
+    char graus [4] = "0";
+    char minutes [3] = "";
+
+    for(i = decimalPointIndex - 1; i >= 0; i--) {
+        if(i >= decimalPointIndex - 2) {
+                switch (decimalPointIndex - i)
+                {
+                case 1:
+                    minutes[1] = latitude[i];
+                    break;
+
+                case 2:
+                    minutes[0] = latitude[i];
+                
+                default:
+                    break;
+                }            
+        }
+
+        else
+        {
+            switch (i)
+            {
+            case 0:
+                graus[0] = latitude[i];
+                break;
+
+            case 1:
+                graus[1] = latitude[i];
+                break;
+
+            case 2:
+                graus[2] = latitude[i];
+                break;
+            
+            default:
+                break;
+            }
+        }        
+    }
+
+    printf("Latitude: %s\'%s%s %cº\n\n", graus, minutes, latitude + decimalPointIndex, polo);
+
+    //longitude
+    for(i = 0; i < strlen(longitude); i ++) {
+        if(longitude[i] == '.') {
+            decimalPointIndex = i;
+        }
+        //se o decimalpoint não for encontrado, é erro de entrada
+    }
+
+    char graus2 [4] = "0";
+    char minutes2 [3] = "";
+
+    for(i = decimalPointIndex - 1; i >= 0; i--) {
+        if(i >= decimalPointIndex - 2) {
+                switch (decimalPointIndex - i)
+                {
+                case 1:
+                    minutes2[1] = longitude[i];
+                    break;
+
+                case 2:
+                    minutes2[0] = longitude[i];
+                
+                default:
+                    break;
+                }            
+        }
+
+        else
+        {
+            switch (i)
+            {
+            case 0:
+                graus2[0] = longitude[i];
+                break;
+
+            case 1:
+                graus2[1] = longitude[i];
+                break;
+
+            case 2:
+                graus2[2] = longitude[i];
+                break;
+            
+            default:
+                break;
+            }
+        }        
+    }
+
+    printf("Longitude: %s\'%s%s %cº\n\n", graus2, minutes2, longitude + decimalPointIndex, eastWest);
+
+    printf("GPS Quality: ");
+    switch (gpsQualityIndicator)
+    {
+    case 0:
+        printf("fix not avaliable\n\n");
+        break;
+
+    case 1:
+        printf("GPS fix\n\n");
+        break;
+
+    case 2:
+        printf("DGPS fix\n\n");
+        break;
+    
+    default:
+        printf("\n\n");
+        break;
+    }
+
+    printf("Number of satellites in use: %s\n\n", numberOfSatellites);
+
+    printf("Horizontal dillution of precision: %s m\n\n", horizontalDilution);
+
+    printf("Altitude: %s %c\n\n", altitude, unitOfAltitude);
+
+    printf("Geoidal Separation: %s %c\n\n", geoidalSeparation, unitOfGeoidalSeparation);
+
+    printf("Age of differential GPS data, time in seconds since last SC104 type 1 or 9 update: ");
+    printf("%s s\n\n", dgpsAge);
+
+    printf("Differential reference station ID: %s\n\n", dgpsID);
+
+    printf("checksum of the sentence received: %s\n", checksum);
+    printf("checksum expected: %x\n", checksumF(sentence + 1));
+    printf("if the values above don't match, the data is invalid\n\n");
+
+
+
+}
+
+int checksumF(char sentence[]) {
+    int sum = 0;
+    int i;
+    for(i = 0; i < strlen(sentence); i++) {
+        if(sentence[i] == '*') {
+            break;
+        }
+        sum = sentence[i] ^ sum;
+    }
+
+    return sum;
 }
 
 
--- decoder.h
+++ decoder.h
@@ -2,6 +2,7 @@ void decode(char[]);
 int gpVer(char  sentence[]);
 char * dataType(char sentence[]);
 void gga(char sentence[]);
+int checksumF(char sentence[]);
 
  
     
